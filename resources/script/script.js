d7d672a1e7fe02f70bfedb1290b825d8726d8c8e
nicolas-riera <nicolas.riera@laplateforme.io>
2026-06-15 10:05:59 +0200

    refactor: added code comments and moved api fetching to the global js script

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,1 +1,20 @@
+// API fetching
+const PUBLIC_KEY = '534084e507ba4b508d43d3a2c176d4a0';
+const AGENDA_ID = '6875632';
+
+const urlParams = new URLSearchParams(window.location.search);
+const eventId = urlParams.get('id');
+
+async function fetchAPI(endpoint = '') {
+    // OpenAgenda is used as a event database
+    const url = `https://api.openagenda.com/v2/agendas/${AGENDA_ID}/events${endpoint}?key=${PUBLIC_KEY}`;
+    
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Erreur HTTP: ${response.status}`);
+    }
+    return response.json();
+}
+
+// Copyright date
 document.getElementById('year').textContent = new Date().getFullYear();
